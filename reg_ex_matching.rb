466714adaf7777f48474d6648bde4650328dea15
Najja <nkossally@gmail.com>
2019-03-16 10:35:20 -0400

    Finish reg ex match.

--- reg_ex_matching.rb
+++ reg_ex_matching.rb
@@ -1,56 +1,49 @@
 require 'byebug'
-def is_match(s,p) 
-  match(s,p,"", []) 
-end
 
-def match(s, p, last_char, arr)
-  
+def is_match(s, p)
+
   if (s == "" && p == "")
     return true 
   end
-
-  if s && p && s[0] == p[0] 
-    if match(s[1..-1], p[1..-1], s[0], [])
-      return true
-    end
+  if p[-1] && p[-1] != "." && p[-1] != "*" && p[-1] != s[-1]
+    return false
   end
-  if p && p[0..1] == ".*"
-    if match(s, p[1..-1], "",("a".."z").to_a)
+  if s  && p && p[0..1] == ".*"
+    if s[0] && is_match(s[1..-1], p)
       return true 
     end
+    if is_match(s, p[2..-1])
+      return true 
+    end
+  end
+
+  if s && p && p[0] != "." && p[1] == "*" 
+    if p[0] == s[0] && is_match(s[1..-1], p)
+      return true 
+    end
+    if is_match(s, p[2..-1])
+      return true 
+    end
+
   end
 
   if p && s && p[0] == "."
-    if( match(s[1..-1],p[1..-1], s[0], []))
+    if( is_match(s[1..-1],p[1..-1]))
+      return true
+    end
+  end
+  if s && p && s[0] == p[0] 
+    if is_match(s[1..-1], p[1..-1])
       return true
     end
   end
 
-  if p && s && p[0] == "*"
-    if (last_char == s[0] || arr.include?(s[0])) 
-      if match(s[1..-1], p[1..-1], s[0], [])
-        return true 
-      elsif match(s[1..-1],p, s[0], arr)
-        return true 
-      end
-    else 
-      if match(s, p[1..-1], last_char, []) 
-        return true 
-      end
-    end
-  end
-
-  if p && p[1] == "*" 
-    if match(s, p[2..-1], "", [])
-      return true 
-    end
-  end
-
-  
   return false 
 end
+p is_match("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c")
+# p is_match("aaa", "ab*a*c*a")
+# p is_match("aa", "a*c*a")
+# p is_match("bbbba", ".*a*a")
 
-p is_match("aaa", "ab*a*c*a")
-# p is_match("a", "*c*a")
 
 
